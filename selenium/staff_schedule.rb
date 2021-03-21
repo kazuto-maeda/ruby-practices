ca91dd4c361d940c108dc619e0744d21c635a673
kazuto-maeda <min6906320@gmail.com>
2021-03-21 19:13:52 +0900

    微調整

--- selenium/staff_schedule.rb
+++ selenium/staff_schedule.rb
@@ -1,11 +1,15 @@
 require 'selenium-webdriver'
+require 'dotenv'
+
+Dotenv.load
 
 @wait_time = 3
 @timeout = 4
 
 # Seleniumの初期化
 # class ref: https://www.rubydoc.info/gems/selenium-webdriver/Selenium/WebDriver/Chrome
-Selenium::WebDriver.logger.output = File.join("./", "selenium.log")
+Selenium::WebDriver.logger.output = File.join(ENV['ROOT_PATH'], "log", "selenium.log")
+
 Selenium::WebDriver.logger.level = :warn
 driver = Selenium::WebDriver.for :chrome
 driver.manage.timeouts.implicit_wait = @timeout
@@ -13,8 +17,6 @@ wait = Selenium::WebDriver::Wait.new(timeout: @wait_time)
 
 # Yahooを開く
 driver.get('https://mysys.work/')
-
-
 
 # ちゃんと開けているか確認するため、sleepを入れる
 sleep 2
